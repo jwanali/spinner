a56e930c6553edfd914f6dd96ef0cba6e270ce32
jwanali <ciwan_ali@hotmail.com>
2023-05-19 12:09:28 -0500

    Spinner II

--- spinner2.js
+++ spinner2.js
@@ -1,43 +1,35 @@
-
 setTimeout(() => {
   process.stdout.write('\r|   ');
 }, 100);
-
 setTimeout(() => {
   process.stdout.write('\r/   ');
 }, 300);
-
 setTimeout(() => {
   process.stdout.write('\r-   ');
 }, 500);
-
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
 setTimeout(() => {
-  
   process.stdout.write('\r|   '); 
+  console.log();
 }, 900);
-setTimeout(() => {
-  
-  process.stdout.write('\r/   '); 
-}, 1100);
-setTimeout(() => {
-  
-  process.stdout.write('\r-   '); 
-}, 1300);
-setTimeout(() => {
-  
-  process.stdout.write('\r\\   '); 
-}, 1500);
-setTimeout(() => {
-  
-  process.stdout.write('\r|   '); 
-}, 1700);
+
+
+
+/*
+let n = 0;
+
+for (let i = 1; i <= 5; i++) {
+  setTimeout(() => {
+    //console.log()
+    process.stdout.write('\r| ');
+  }, n);
+  n += 200;
+}
 setTimeout(() => {
   //console.log()
   process.stdout.write('\n');
 },1900);
 
-
+*/
